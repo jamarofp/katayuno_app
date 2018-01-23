cdfd8873a1dd5e64b8400395456c0516d3aa8777
ja_ma <ja_ma_ro@hotmail.com>
2018-01-23 20:27:37 +0100

    Add create functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'katas#index'
 
-  get '/kata/:id' => 'katas#show', as: :kata # Se enviá al controller katas > show, y se usa as: :kata como kata_path del index.html.erb
+  get '/katas/:id' => 'katas#show', as: :kata # Se enviá al controller katas > show, y se usa as: :kata como kata_path del index.html.erb
+  get '/new/katas' => 'katas#new', as: :new_kata
+  post '/create/katas' => 'katas#create', as: :create_katas
+  
 
 end
