388fc14a325cca0e2e9819a9118d7a2fa163f450
ja_ma <ja_ma_ro@hotmail.com>
2018-01-23 19:07:10 +0100

    Add method create_kata and second_kata in spec

--- spec/features/kata_spec.rb
+++ spec/features/kata_spec.rb
@@ -6,16 +6,35 @@ describe 'Kata' do
     kata_title = 'Kata title'
     kata_description = 'Kata description'
 
-    kata = Kata.new(
+    create_kata(
       title: kata_title,
       description: kata_description
     )
-    kata.save
 
     visit root_path
     click_on(kata_title)
 
     expect(page).to have_content(kata_title)
     expect(page).to have_content(kata_description)
+  end
+
+  it 'can be read (two katas)' do
+    second_title = 'Second title'
+    second_description = 'Second description'
+    create_kata
+    create_kata(title: second_title, description: second_description)
+
+    visit root_path
+    click_on(second_title)
+
+    expect(page).to have_content(second_title)
+    expect(page).to have_content(second_description)
+  end
+
+  def create_kata(title: 'Kata_title', description: `Kata_description`)
+    kata = Kata.new(title: title, description: description)
+    kata.save
+
+    kata
   end
 end
